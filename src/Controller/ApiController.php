2ed666c4a4653f5b634c623b9ef5f02e92437b23
NatChe <natche@MacBook-Pro-de-NatChe.local>
2019-03-02 13:04:01 +0100

    Install web-server-bundle for better performance

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: natche
+ * Date: 02/03/2019
+ * Time: 12:54
+ */
+
+namespace App\Controller;
+
+
+class ApiController
+{
+
+}
---
+++ src/Controller/DoughnutController.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: natche
+ * Date: 02/03/2019
+ * Time: 12:46
+ */
+class DoughnutController
+{
+
+}
